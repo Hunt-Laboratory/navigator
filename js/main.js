0980d4eedc58f6b822cba89c8ed6b830ca982b59
Luke Thorburn <luke.thorburn@gmail.com>
2020-11-25 14:13:40 +1100

    Fixed relative paths

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const {neverland: $, render, html, useState} = window.neverland;
 
 import Router from './components/Router.js';
 
-fetch('../data/demo/democratic-primary-2016.json')
+fetch('data/demo/democratic-primary-2016.json')
   .then(response => response.json())
   .then(result => {
 	
